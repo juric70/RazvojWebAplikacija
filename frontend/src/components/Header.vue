b87505ae90e92abc16c35efb7e377dcecb5e70d6
juric70 <andjela.juric12@gmail.com>
2022-11-29 22:02:15 +0100

    Making header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,93 @@
+<template>
+
+<header>
+    <img id="headerImg" :src="require('../assets/logoOfClub.png')"/>
+    <div id="headerMenuDiv">
+    <a href="#" class="headerItem">Novosti</a>
+    <a href="#" class="headerItem">Treninzi</a>
+    <a href="#" class="headerItem">Raspored</a>
+    <a href="#" class="headerItem">Treneri</a>
+    <a href="#" class="headerItem">Cjenik</a>
+    <a href="#" class="headerItem">Album</a>
+    <a href="#" class="headerItem">Podrška</a>
+    <a href="#" class="headerItem">O nama</a>   
+  </div>
+  <div id="headerIconDiv">
+  
+    <div id="headerIcon">
+        <img class="headerIcon" :src="require('../assets/profilePictureIcon.png')"/>
+        <img class="headerIcon" :src="require('../assets/SearchIcon.png')"/>
+    </div>
+    <div id="searchInputDiv">
+        <input type="text" v-model="text" id="searchInput">
+    </div>
+  </div>
+      
+
+
+</header>
+
+</template>
+
+<script>
+export default{
+    name: 'Header'
+}
+</script>
+
+<style lang="css">
+#headerImg{
+    width: 60px;
+    height: 60px;
+    float: left;
+}
+header{
+  font-weight: bold;
+  background-color:  #007399;
+  height: 58px;
+  overflow: hidden;
+}
+header a{
+  display:inline-block;
+  padding:20px 15px;
+  color: #333333;
+  text-decoration: none;
+}
+#headerMenuDiv{
+  margin: auto;
+  width: 70%;
+  padding: 10px;
+  display: inline;
+}
+header a:hover{
+    color:  white;
+
+}
+#headerIconDiv{
+    display: inline;
+}
+.headerIcon{
+    width: 25px;
+    height: 25px;
+    float: right;
+    padding:17px 15px;
+}
+#headerIcon{
+
+    display: inline;
+    float: right;
+}
+#searchInputDiv{
+    padding:15px 15px;
+    display: inline;
+    margin-left: px;
+    float: right;
+
+}
+#searchInput{
+    width: 200px;
+    height: 25px;
+    border-radius: 15px;
+    border: 1px solid lightgrey;
+}
+</style>
